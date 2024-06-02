88fc67112af019959e96f5bc992241aefed09d08
sagarmaiti26 <sagarmaiti277@gmail.com>
2024-06-02 09:52:05 +0530

    changes

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/libs/AuthOptions";
+import { authOptions } from "../libs/AuthOptions";
 export default async function getSession(){
     return await getServerSession(authOptions);
 }
