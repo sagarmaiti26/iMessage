5acd6fb1fa590c61b4aa73b0c03f8c6d55489aff
Sagar Maiti <122274356+sagarmaiti26@users.noreply.github.com>
2024-06-02 10:05:21 +0530

    name conflict changes

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../libs/AuthOptions";
+import { authOptions } from "@/app/libs/AuthOptions";
 export default async function getSession(){
     return await getServerSession(authOptions);
 }
